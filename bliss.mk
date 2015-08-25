bcd7fc5d07bb12e305c0552116ad1b7d6697a913
sub77 <sub77@ymail.com>
2015-08-25 02:01:44 +0200

    update

--- bliss.mk
+++ bliss.mk
@@ -14,6 +14,7 @@
 
 # Inherit some common Bliss stuff.
 $(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
+#$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
--- full_matissewifi.mk
+++ full_matissewifi.mk
@@ -14,6 +14,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from matissewifi device
 $(call inherit-product, device/samsung/matissewifi/device.mk)
